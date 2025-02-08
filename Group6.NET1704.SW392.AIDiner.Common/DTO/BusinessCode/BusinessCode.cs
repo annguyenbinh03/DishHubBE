e251d5682b9b08f6fbe93c04585d34a3c8590afa
nguyenvantoanpr359 <nguyenvantoanpr359@gmail.com>
2025-02-08 14:17:31 +0700

    create responsedto & businesscode

---
+++ Group6.NET1704.SW392.AIDiner.Common/DTO/BusinessCode/BusinessCode.cs
@@ -0,0 +1,14 @@
+namespace Group6.NET1704.SW392.AIDiner.Common.DTO.BusinessCode;
+
+public enum BusinessCode
+{
+    GET_DATA_SUCCESSFULLY = 2000,
+    EXCEPTION = 4000,
+    SIGN_UP_SUCCESSFULLY = 2001,
+    SIGN_UP_FAILED = 2002,
+    EXISTED_USER = 2003,
+    AUTH_NOT_FOUND = 401,
+    WRONG_PASSWORD = 405,
+    ACCESS_DENIED = 403,
+    INSERT_SUCESSFULLY = 2005
+}
---
+++ Group6.NET1704.SW392.AIDiner.Services/Contract/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group6.NET1704.SW392.AIDiner.Services.Contract
+{
+    internal interface ICategoryService
+    {
+    }
+}
