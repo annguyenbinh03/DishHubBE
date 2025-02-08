496ce3480664d20afd7a25831440df4e632bd314
nguyenvantoanpr359 <nguyenvantoanpr359@gmail.com>
2025-02-08 17:57:15 +0700

    commit code fixgetallcategory

---
+++ Group6.NET1704.SW392.AIDiner.Common/DTO/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group6.NET1704.SW392.AIDiner.Common.DTO
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public string Image { get; set; }
+    }
+}
