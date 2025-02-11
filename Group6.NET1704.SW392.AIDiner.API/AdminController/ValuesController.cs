51f835c8db798db116d29ed5d1a518a946503647
nguyenvantoanpr359 <nguyenvantoanpr359@gmail.com>
2025-02-11 22:56:32 +0700

    Commit code

---
+++ Group6.NET1704.SW392.AIDiner.API/AdminController/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group6.NET1704.SW392.AIDiner.API.AdminController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
