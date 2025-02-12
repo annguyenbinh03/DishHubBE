319e8fa86ecc1ee0b31c9de66d9e549edd6bad63
Hung <khanhhungzm@gmail.com>
2025-02-12 18:52:03 +0700

    add api auth me

--- Group6.NET1704.SW392.AIDiner.API/UserControllers/UserController.cs
+++ Group6.NET1704.SW392.AIDiner.API/UserControllers/UserController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Group6.NET1704.SW392.AIDiner.Common.DTO;
+using Group6.NET1704.SW392.AIDiner.Common.DTO.BusinessCode;
 using Group6.NET1704.SW392.AIDiner.Services.Contract;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,8 +22,23 @@ namespace FS.HotelBooking.API.Controllers
             _service = service;
         }
 
-        
+        [HttpGet]
+        public async Task<ResponseDTO> GetUserFromLogin()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-       
+            if (string.IsNullOrEmpty(userId) || !int.TryParse(userId, out var parsedUserId))
+            {
+                return new ResponseDTO
+                {
+                    IsSucess = false,
+                    Data = null,
+                    BusinessCode = BusinessCode.NOT_FOUND,
+                };
+            }
+
+            return await _service.GetUserById(parsedUserId);
+        }
+
     }
 }
