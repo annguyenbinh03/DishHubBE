c4cf8e738a5b47384c7ecb41f1221fe1ca7d0748
nguyenvantoanpr359 <nguyenvantoanpr359@gmail.com>
2025-02-22 14:04:09 +0700

    commit code gettableforadmin + createTableforAdmin

---
+++ Group6.NET1704.SW392.AIDiner.Common/DTO/AdminDTO/CreateTableForAdminDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group6.NET1704.SW392.AIDiner.Common.DTO.AdminDTO
+{
+    public class CreateTableForAdminDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int RestaurantId { get; set; }
+    }
+
+}
